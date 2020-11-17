f52bacf35314dadb421f2c6a8abb7e5954706240
Jake <jakekippes123gmail.com>
2020-11-17 10:22:19 -0600

    final commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,27 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let length = Number(prompt("Enter a password length between 8 and 128")),
+charType = prompt("Enter a character type: special, numeric, uppercase, lowercase."),
 
-// Write password to the #password input
+let generateBtn = document.querySelector("#generate");
+let password = generatePassword();
+let passwordText = document.querySelector("#password");
+ passwordText.value = password;
+
+
+// Generates random password
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  let charSets = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numeric: "0123456789",
+    special: "!#$%&\()*+,-./:;<=>?@[\\]^_`{|}~"
+  };
+  let charSet = charSets[charType.toLowerCase()] || charSets.lowercase;
+  let retVal = "";
+  for (var i = 0; i < length; i++) {
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
 }
 
 // Add event listener to generate button
